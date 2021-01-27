9fb6297b622d920220a2ff240e7889ad8b6d553d
daniela-cruz <danigoel@gmail.com>
2021-01-27 12:24:04 +0200

    Fetch app

--- 06_routing/routing/src/App.js
+++ 06_routing/routing/src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
+import FetchRandomUser from "./FetchRandomUser";
 
-  useEffect(() => {    document.title = `You clicked ${count} times`;  });
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <FetchRandomUser />
+      </div>
+    );
+  }
 }
+
 export default App;
